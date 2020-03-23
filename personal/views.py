22d3cad85c9c6fd4edfe88c6ada42eec1e61c4c5
yegor <yegor17@gmail,com>
2020-03-23 13:38:25 +0300

    Django Models and Django Admin

--- personal/views.py
+++ personal/views.py
@@ -1,23 +1,11 @@
 from django.shortcuts import render
+from personal.models import Question 
 
 # Create your views here.
 
 def home_screen_view(request):
 	context = {}
-	# context['some_string'] = "this is a some string, that i'm passing to the view"
-	# context['some_number'] = 123432
-
-	# context = {
-	# 	'some_string': "какая-то строчка",
-	# 	'some_number': 1234
-	# }
-
-	list_of_value = []
-	list_of_value.append("first_entry")
-	list_of_value.append("second_entry")
-	list_of_value.append("third_entry")
-	list_of_value.append("fourth_entry")
-
-	context['list'] = list_of_value
+	questions = Question.objects.all()
+	context["questions"] = questions
 
 	return render(request, "personal/home.html", context)
